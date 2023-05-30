d9e0205acd1ef193fab3f5f4839d582f5ba5a1f5
amir-kaaa <miramirkaa@gmail.com>
2023-05-30 08:58:36 +0400

    jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     env: {
         browser: true,
         es2021: true,
+        jest: true,
     },
     extends: [
         'plugin:react/recommended',
@@ -39,6 +40,7 @@ module.exports = {
         'no-underscore-dangle': 'off',
         '@typescript-eslint/no-shadow': 'warn',
         'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'ignoreComments': true
     },
     globals: {
         __IS_DEV__: true,
