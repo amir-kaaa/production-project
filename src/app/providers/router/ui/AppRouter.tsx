5473200008c9bba81b3834006b833c81c798ba48
amir-kaaa <miramirkaa@gmail.com>
2023-06-02 16:27:53 +0400

    page loader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,24 +1,25 @@
 import { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader/PageLoader';
 
 const AppRouter = () => (
-    // eslint-disable-next-line i18next/no-literal-string
-    <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-            {Object.values(routeConfig).map(({ element, path }) => (
-                <Route
-                    key={path}
-                    path={path}
-                    element={(
+    <Routes>
+        {Object.values(routeConfig).map(({ element, path }) => (
+            <Route
+                key={path}
+                path={path}
+                element={(
+                    // eslint-disable-next-line i18next/no-literal-string
+                    <Suspense fallback={<PageLoader />}>
                         <div className="page-wrapper">
                             {element}
                         </div>
-                    )}
-                />
-            ))}
-        </Routes>
-    </Suspense>
+                    </Suspense>
+                )}
+            />
+        ))}
+    </Routes>
 );
 
 export default AppRouter;
