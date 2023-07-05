afe792ce370d8f3d578cdad6135848719bb69726
Amir <miramirkaa@gmail.com>
2023-07-05 21:44:25 +0400

    libraries updated, storybook init and bug fixed

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
-import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
+import { createRoot } from 'react-dom/client';
 import App from './app/App';
 import 'shared/config/i18n/i18n';
 
-render(
+const root = createRoot(document.getElementById('root'));
+root.render(
     <BrowserRouter>
         <ErrorBoundary>
             <ThemeProvider>
@@ -13,5 +14,4 @@ render(
             </ThemeProvider>
         </ErrorBoundary>
     </BrowserRouter>,
-    document.getElementById('root'),
 );
---
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -0,0 +1,10 @@
+// import { StoryObj } from '@storybook/react';
+// import { Theme } from 'app/providers/ThemeProvider';
+
+// type Story = StoryObj<typeof Theme>;
+
+// export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) => (
+//     <div className={`app ${theme}`}>
+//         <StoryComponent />
+//     </div>
+// );
