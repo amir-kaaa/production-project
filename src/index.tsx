627f670fa4b58c33f22f776516d572e8732886e2
Amir <miramirkaa@gmail.com>
2023-08-01 20:10:22 +0400

    i18n bug fix

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from 'app/providers/ThemeProvider';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 import { createRoot } from 'react-dom/client';
 import App from './app/App';
-import 'shared/config/i18n/i18n';
+import './shared/config/i18n/i18n';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
