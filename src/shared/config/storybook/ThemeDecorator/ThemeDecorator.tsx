c231e7cd77ba88f5315bd1791d24502c47409ca1
Amir <miramirkaa@gmail.com>
2023-10-16 01:20:25 +0400

    modal

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -1,10 +1,10 @@
 // import { StoryObj } from '@storybook/react';
 // import { Theme } from 'app/providers/ThemeProvider';
 
-// type Story = StoryObj<typeof Theme>;
-
-// export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) => (
-//     <div className={`app ${theme}`}>
-//         <StoryComponent />
-//     </div>
-// );
+export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) => (
+    <ThemeProvider initialTheme={theme}>
+        <div className={`app ${theme}`}>
+            <StoryComponent />
+        </div>
+    </ThemeProvider>
+);
