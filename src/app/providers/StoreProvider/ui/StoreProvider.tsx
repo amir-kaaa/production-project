a234a252f2928e9fee59e45173445188bed82618
Amir <miramirkaa@gmail.com>
2023-11-11 20:25:41 +0400

    tests config debug

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -1,11 +1,12 @@
 import { ReactNode } from 'react';
 import { Provider } from 'react-redux';
-import { createReduxStore } from '../config/store';
-import { StateSchema } from '../config/StateSchema';
+import { createReduxStore } from 'app/providers/StoreProvider/config/store';
+import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
+import { DeepPartial } from '@reduxjs/toolkit';
 
 interface StoreProviderProps {
     children?: ReactNode;
-    initialState?: StateSchema;
+    initialState?: DeepPartial<StateSchema>;
 }
 
 export const StoreProvider = (props: StoreProviderProps) => {
@@ -14,7 +15,7 @@ export const StoreProvider = (props: StoreProviderProps) => {
         initialState,
     } = props;
 
-    const store = createReduxStore(initialState);
+    const store = createReduxStore(initialState as StateSchema);
 
     return (
         <Provider store={store}>
