5c77c782c470ac4575ae66be4b980d4286e0bcf1
Amir <miramirkaa@gmail.com>
2023-11-13 22:21:04 +0400

    global theme bug fixed

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -5,6 +5,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import React, {
     ReactNode, useCallback, useEffect, useRef, useState,
 } from 'react';
+import { useTheme } from 'app/providers/ThemeProvider';
 import cls from './Modal.module.scss';
 import { Portal } from '../Portal/Portal';
 
@@ -22,6 +23,7 @@ export const Modal = (props: ModalProps) => {
 
     const [isClosing, setIsClosing] = useState(false);
     const timerRef = useRef<ReturnType<typeof setTimeout>>();
+    const { theme } = useTheme();
     const ANIMATION_DELAY = 150;
 
     const closeHandler = useCallback(() => {
@@ -61,7 +63,7 @@ export const Modal = (props: ModalProps) => {
     };
     return (
         <Portal>
-            <div className={classNames(cls.Modal, mods, [className])}>
+            <div className={classNames(cls.Modal, mods, [className, theme, 'app_modal'])}>
                 <div className={cls.overlay} onClick={closeHandler}>
                     <div className={cls.content} onClick={contentClickHandler}>
                         {children}
